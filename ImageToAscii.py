c298129b0713f6505ee7aafc856af1f340625a1a
Spec.psx <69973760+Spectrewolf8@users.noreply.github.com>
2023-02-19 14:42:22 +0500

    Made video-ascii converter work, need to test splitter now

---
+++ ImageToAscii.py
@@ -0,0 +1,45 @@
+from PIL import Image
+
+ascii_characters_by_surface = ".X"#"`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+
+def main():
+    image = Image.open('TestTroll.png')
+    # you can first resize the image if needed
+    image = image.resize((48, 36))
+    ascii_art = convert_to_ascii_art(image)
+    save_as_text(ascii_art)
+
+
+def convert_to_ascii_art(image):
+    ascii_art = []
+    (width, height) = image.size
+    for y in range(0, height - 1):
+        line = ''
+        for x in range(0, width - 1):
+            px = image.getpixel((x, y))
+            print(px)
+            line += convert_pixel_to_character(px)
+        ascii_art.append(line)
+    return ascii_art
+
+
+def convert_pixel_to_character(pixel):
+    (r, g) = pixel
+    pixel_brightness = r + g
+    max_brightness = 255 * 3
+    brightness_weight = len(ascii_characters_by_surface) / max_brightness
+    index = int(pixel_brightness * brightness_weight) - 1
+    return ascii_characters_by_surface[index]
+
+
+def save_as_text(ascii_art):
+    with open("image.txt", "w") as file:
+        for line in ascii_art:
+            file.write(line)
+            file.write('\n')
+        file.close()
+
+
+if __name__ == '__main__':
+    main()
