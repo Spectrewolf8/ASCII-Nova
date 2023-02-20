f8085b2e73171c526f13a3c25bcd42ac4102a9ed
Spec.psx <69973760+Spectrewolf8@users.noreply.github.com>
2023-02-20 20:58:58 +0500

    Screen renderer completed

--- RenderScreen.py
+++ RenderScreen.py
@@ -1,25 +1,32 @@
 import pygame
-from pygame import DOUBLEBUF, RESIZABLE, HWSURFACE, QUIT, VIDEORESIZE
+from pygame import DOUBLEBUF, RESIZABLE, HWSURFACE, QUIT, VIDEORESIZE, display
+
+pygame.init()
+pygame.font.init()
 
 background_colour = (30, 30, 30)
 
 # screen = pygame.display.set_mode((1024, 768), pygame.RESIZABLE, vsync=1)
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
 screen = pygame.display.set_mode((1024, 768), HWSURFACE | DOUBLEBUF | RESIZABLE)
 pygame.display.set_caption('BadApplePlayer')
 screen.fill(background_colour)
-
 pygame.display.flip()
 
-running = True
+myfont = pygame.font.Font('courier.ttf', 32)
 
+running = True
+i = 0
 # game loop
 while running:
     screen.fill(background_colour)
-    # for loop through the event queue
+    textToRender = myfont.render(str(i), True, (255, 255, 255))
+    i += 1
+    screen.blit(textToRender, textToRender.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
+    display.flip()
     for event in pygame.event.get():
 
         # Check for QUIT event
         if event.type == QUIT:
-            pygame.display.quit()
-        elif event.type == VIDEORESIZE:
-            pygame.display.flip()
+            display.quit()
