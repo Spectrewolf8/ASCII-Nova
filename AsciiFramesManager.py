8d722c6b5aa36674fa2bff079d0decf4544f9f0c
Spec.psx <69973760+Spectrewolf8@users.noreply.github.com>
2023-02-21 00:23:20 +0500

    json parsing and asciiframes manager is yet to be completed but oh well it's time to sleep

--- AsciiFramesManager.py
+++ AsciiFramesManager.py
@@ -1,6 +1,5 @@
 import natsort
 import time
-
 import ImageToAscii as imageToAscii
 import os
 import cv2
@@ -17,12 +16,14 @@ print(frames)
 
 initial_time = time.time()
 asciiFramesBuffer = []
+
 for frame in frames:
     wholeFrame = ""
     for ascii_rowns in imageToAscii.convert_Image_To_Ascii(("FramesToConvertToAscii/" + frame),
-                                                           (round(width / 4), round(height / 4))):
+                                                           (round(48), round(36))):
         wholeFrame += "\n" + ascii_rowns
     asciiFramesBuffer.append(wholeFrame)
+
     print(wholeFrame)
     print('Generated ' + str(frames.index(frame)) + "th frame")
 
--- RenderScreen.py
+++ RenderScreen.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame import DOUBLEBUF, RESIZABLE, HWSURFACE, QUIT, VIDEORESIZE, display
+from pygame import DOUBLEBUF, RESIZABLE, HWSURFACE, QUIT, display
 
 pygame.init()
 pygame.font.init()
@@ -7,23 +7,27 @@ pygame.font.init()
 background_colour = (30, 30, 30)
 
 # screen = pygame.display.set_mode((1024, 768), pygame.RESIZABLE, vsync=1)
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
-screen = pygame.display.set_mode((1024, 768), HWSURFACE | DOUBLEBUF | RESIZABLE)
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), HWSURFACE | DOUBLEBUF | RESIZABLE)
 pygame.display.set_caption('BadApplePlayer')
 screen.fill(background_colour)
 pygame.display.flip()
-myfont = pygame.font.Font('courier.ttf', 32)
+ascii_render_font = pygame.font.Font('courier.ttf', 32)
+fps_count_render_font = pygame.font.Font('courier.ttf', 20)
+
 # myfont = pygame.font.SysFont('Courier Prime Regular', 32, bold=False, italic=False)
 
 running = True
 
 clock = pygame.time.Clock()
 
+FPS_LOCK_VALUE = 500
+
 
 def show_fps():
     fps_text = str(int(clock.get_fps()))
-    fps_surface = myfont.render(fps_text, True, (255, 255, 255))
+    fps_surface = fps_count_render_font.render(('fps: ' + fps_text), True, (255, 255, 255))
     screen.blit(fps_surface, fps_surface.get_rect())
 
 
@@ -33,10 +37,10 @@ def mainloop():
     while running:
         i += 1
         screen.fill(background_colour)  # filling background on resize
-        textToRender = myfont.render(str(i), True, (255, 255, 255))
+        textToRender = ascii_render_font.render(str(i), True, (255, 255, 255))
         screen.blit(textToRender, textToRender.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)))
 
-        clock.tick(30)  # making fps constant 30
+        clock.tick(FPS_LOCK_VALUE)  # making fps constant 30
         show_fps()  # to display fps in top left
         display.flip()  # to update display
         for event in pygame.event.get():
