5fbb1547235319619a5e5abda8649a0e4ea60e2b
spectrewolf8 <salmantariq8385@gmail.com>
2023-06-27 23:28:24 +0500

    Some functional changes were added to the code

--- ImageToAscii.py
+++ ImageToAscii.py
@@ -1,41 +1,45 @@
 from PIL import Image
 
-
-# def image_to_ascii_art(image, asciiRenderWidth=None, ASCII_CHARS=None):
-#     if ASCII_CHARS is None:
-#         ASCII_CHARS = default_ASCII_CHARS
-#     pixels = image.getdata()
-#     # replace each pixel with a character from array
-#     new_pixels = [ASCII_CHARS[pixel // 25] for pixel in pixels]
-#     new_pixels = ''.join(new_pixels)
-#     if asciiRenderWidth is None:
-#         new_width = 120
-#     else:
-#         new_width = asciiRenderWidth
-#     # split string of chars into multiple strings of length equal to new width and create a list
-#     new_pixels_count = len(new_pixels)
-#     ascii_image_string = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
-#     ascii_image_string = "\n".join(ascii_image_string)
-#     return ascii_image_string
+default_ASCII_CHARS = ["@", "#", "＄", "%", "?", "*", "+", ";", ":", ",", "."]
 
 
 def image_to_ascii_art(image, asciiRenderWidth=None, ASCII_CHARS=None):
     if ASCII_CHARS is None:
-        ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]  # Default ASCII characters
+        ASCII_CHARS = default_ASCII_CHARS
     pixels = image.getdata()
-    num_chars = len(ASCII_CHARS)
-    char_width = max(1, 256 // num_chars)  # Calculate the width of each ASCII character range
-    new_pixels = [ASCII_CHARS[pixel // char_width] for pixel in
-                  pixels]  # Replace each pixel with a character from the array
+    # replace each pixel with a character from array
+    new_pixels = [ASCII_CHARS[pixel // 25] for pixel in pixels]
     new_pixels = ''.join(new_pixels)
     if asciiRenderWidth is None:
         new_width = 120
     else:
         new_width = asciiRenderWidth
+    # split string of chars into multiple strings of length equal to new width and create a list
     new_pixels_count = len(new_pixels)
     ascii_image_string = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
     ascii_image_string = "\n".join(ascii_image_string)
+
     return ascii_image_string
+
+
+# def image_to_ascii_art(image, asciiRenderWidth=None, ASCII_CHARS=None):
+#     if ASCII_CHARS is None:
+#         ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]  # Default ASCII characters
+#     pixels = image.getdata()
+#     print(pixels)
+#     num_chars = len(ASCII_CHARS)
+#     char_width = max(1, 256 // num_chars)  # Calculate the width of each ASCII character range
+#     new_pixels = [ASCII_CHARS[pixel // char_width] for pixel in
+#                   pixels]  # Replace each pixel with a character from the array
+#     new_pixels = ''.join(new_pixels)
+#     if asciiRenderWidth is None:
+#         new_width = 120
+#     else:
+#         new_width = asciiRenderWidth
+#     new_pixels_count = len(new_pixels)
+#     ascii_image_string = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
+#     ascii_image_string = "\n".join(ascii_image_string)
+#     return ascii_image_string
 
 
 def resize_for_ascii(image_path, asciiRenderWidth=None):
